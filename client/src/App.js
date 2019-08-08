69af8f1419cc222ce6870596c62498084c213b50
Amanda Dovel <amandacdovel@gmail.com>
2019-08-07 20:50:21 -0600

    fixing heroku

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import { Container } from "./components/Grid";
-import Search from "./pages/Search";
+// import Search from "./pages/Search";
 import Saved from "./pages/Saved";
 import NoMatch from "./pages/NoMatch";
 
@@ -13,7 +13,7 @@ const App = () => (
             <Navbar />
             <Container>
                 <Switch>
-                    <Route exact path="/" component={Search} />
+                    {/* <Route exact path="/" component={Search} /> */}
                     <Route exact path="/saved" component={Saved} />
                     <Route component={NoMatch} />
                 </Switch>
